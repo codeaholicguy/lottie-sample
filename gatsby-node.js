cab9908460835598c767169749372797650ea0a2
Hoang Nhat Nguyen <hoangnn93@gmail.com>
2018-05-09 00:36:14 +0700

    Update config for third party library

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,11 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-exports.modifyWebpackConfig = ({config, stage}) => {
-  if (stage === 'build-html') {
-    config.loader('null', {
-      test: /lottie/,
-      loader: 'null-loader',
-    })
-  }
-}
